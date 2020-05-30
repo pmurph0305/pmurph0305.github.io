5c68698ee2782aa631360cf51f8e921678d819fb
unknown <pmurph0305@gmail.com>
2020-05-30 12:59:05 -0400

    Modify styles for small screens

--- scrollAnimations.js
+++ scrollAnimations.js
@@ -1,11 +1,11 @@
 let options = {
   root: null,
   rootMargin: "0px",
-  threshold: [0.1, 0.75]
+  threshold: [0.1, 0.75],
 };
 
 let observerCallback = (entries, observer) => {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("animate-visible");
     }
@@ -15,9 +15,9 @@ let observerCallback = (entries, observer) => {
 let observer = new IntersectionObserver(observerCallback);
 
 let scrollElements = document.querySelectorAll(".animate-on-scroll");
-scrollElements.forEach(element => {
+scrollElements.forEach((element) => {
   observer.observe(element);
-  element.addEventListener("animationend", function() {
+  element.addEventListener("animationend", function () {
     if (element.classList.contains("reanimate")) {
       element.classList.remove("animate-visible");
     }
